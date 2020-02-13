c59f9eae81b78067f95f26a35e5443b51d9cd375
Douglas Miner <curr3nas@gmail.com>
2020-02-13 14:56:44 -0600

    added minimal css to identify different pages

--- app.js
+++ app.js
@@ -1,47 +1,95 @@
-'use strict';
-
 /**
  * Example store structure
  */
+'use strict';
 
 const store = {
   // 5 or more questions are required
   questions: [
     {
-      question: 'What color is broccoli?',
-      answers: [
-        'red',
-        'orange',
-        'pink',
-        'green'
+      question: 'What is the Capital of India?',
+      answer: [
+        'Bangladesh',
+        'New Delhi',
+        'Caracas',
+        'Tehran'
       ],
-      correctAnswer: 'green'
+      correctAnswer: 'New Delhi'
     },
     {
-      question: 'What is the current year?',
+      question: 'What group of animals is called a caravan?',
       answers: [
-        '1970',
-        '2015',
-        '2019',
-        '2005'
+        'Camels',
+        'Horses',
+        'Giraffes',
+        'Zebras'
       ],
-      correctAnswer: '2019'
+      correctAnswer: 'Camels'
+    },
+    {
+      question: 'What year did Steve Jobs die?',
+      answers: [
+        '2019',
+        '2015',
+        '2003',
+        '2012'
+      ],
+      correctAnswer: '2012'
+    },
+    {
+      question: 'What tech company bought Columbia Pictures?',
+      answers: [
+        'Sony',
+        'Apple',
+        'Microsoft',
+        'Panasonic'
+      ],
+      correctAnswer: 'Sony'
+    },
+    {
+      question: 'What planet has the strongest gravity?',
+      answers: [
+        'Jupiter',
+        'Venus',
+        'Pluto',
+        'Saturn'
+      ],
+      correctAnswer: 'Saturn'
     }
   ],
   quizStarted: false,
   questionNumber: 0,
   score: 0
 };
+  
+function generateAnswerList(answers){
+  //template generator 1
+}
+
+function renderQuestionText(){
+  //HTML templates go here and called back
+}
+
+// Event handlers
+function handleAnswerSubmitted() {
+  $('.user-controls').on('click', '.submit-answer', () => {
+    // Retrieve answer identifier of user-checked radio btn
+    // Perform check: User answer === Correct answer?
+    // Update STORE and render appropriate section
+  });
+}
+  
+$(handleAnswerSubmitted);
 
 /**
- *
- * Your app should include a render() function, that regenerates
- * the view each time the store is updated. See your course
- * material, consult your instructor, and reference the slides
- * for more details.
- *
- * NO additional HTML elements should be added to the index.html file.
- *
- * You may add attributes (classes, ids, etc) to the existing HTML elements, or link stylesheets or additional scripts if necessary
- *
- */
+   *
+   * Your app should include a render() function, that regenerates
+   * the view each time the store is updated. See your course
+   * material, consult your instructor, and reference the slides
+   * for more details.
+   *
+   * NO additional HTML elements should be added to the index.html file.
+   *
+   * You may add attributes (classes, ids, etc) to the existing HTML elements, or link stylesheets or additional scripts if necessary
+   *
+   */
