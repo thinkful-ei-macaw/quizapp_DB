5906b6a0ae63b762caaa5fad6a0e02eac57cd57e
Douglas Miner <curr3nas@gmail.com>
2020-02-13 19:25:45 -0600

    start quiz button now produces first question view

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
+/* eslint-disable no-console */
 /**
  * Example store structure
  */
 'use strict';
+
+// store.questions[0].answers[0]
 
 const store = {
   // 5 or more questions are required
@@ -70,17 +73,45 @@ function generateAnswerList(answers){
 
 
 function renderStart(){
-  console.log('Generating start view')
+  console.log('Generating start view');
   const start = `
   <div id="start-view">
-  <form id="js-start-quiz-form"></form>
+  <form id="js-start-quiz">
   <button type="submit">Start Quiz</button>
+  </form>
 </div>`;
   $('.js-quiz-app').html(start);
 }
 
 function renderQuestionText(){
-  //HTML templates go here and called back
+  console.log('Generating questions view')
+  for (let i = 0; i < store.questions.length; i++) {
+    
+    let currentScore = 0;
+    let currentQuestion = 1;
+    const questions = `
+    <div id="question-view">
+        <ul>
+          <li>${currentQuestion} of ${store.questions.length}</li>
+          <li>${currentScore}/${store.questions.length}</li>
+        </ul>
+        
+        <form action="/action_page.php">
+          <p>${store.questions[0].question}</p>
+          <input type="radio" id="A" name="answer" value="answer A">
+          <label for="A">${store.questions[0].answers[0]}</label>
+          <input type="radio" id="B" name="answer" value="answer B">
+          <label for="B">${store.questions[0].answers[1]}</label>
+          <input type="radio" id="C" name="answer" value="answer C">
+          <label for="C">${store.questions[0].answers[2]}</label>
+          <input type="radio" id="D" name="answer" value="answer A">
+          <label for="D">${store.questions[0].answers[3]}</label>
+        </form>
+        <form id="js-question-submit"></form>
+        <button type="submit">Submit Answer</button>
+      </div>`;
+      $('.js-quiz-app').html(questions);
+  }
 }
 
 // Event handlers
@@ -93,9 +124,20 @@ function handleAnswerSubmitted() {
   });
 }
 
+function handleQuizStart() {
+  $('.js-quiz-app').on('submit', '#js-start-quiz', function(event) {
+    event.preventDefault();
+    console.log('`handleQuizStart` ran');
+    renderQuestionText();
+  });
+}
+
 function handleQuiz() {
   renderStart();
+  handleQuizStart();
 }
+
+
 $(handleQuiz);
 $(handleAnswerSubmitted);
 
